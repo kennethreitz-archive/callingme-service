d1dff4cbb8fd6f54e362563718a5db96ad6da977
Kenneth Reitz <me@kennethreitz.com>
2012-07-09 02:29:23 -0400

    description

--- rush/core.py
+++ rush/core.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, jsonify
+
 from flask_heroku import Heroku
 from flask_sslify import SSLify
 from raven.contrib.flask import Sentry
@@ -20,6 +21,27 @@ app.debug = True
 @app.route('/')
 def describe_api():
     """Returns a description of the API."""
-    return 'yay!'
+
+    descr = {
+        'resources': {
+            '/numbers/:number': 'Returns data about the given number.',
+            '/areas/:area': 'Returns data about a given area code.',
+            '/areas/:area/:exchange': 'Returns data about a given exchange.',
+            '/normalize?number=:number': 'Returns a normalized Phone number.'
+        }
+    }
+    return jsonify(descr)
 
 @app.route('/numbers/<number>')
+def function():
+    pass
+
+
+def area_code():
+    pass
+
+def area_exchange():
+    pass
+
+def normalize_number():
+    pass
